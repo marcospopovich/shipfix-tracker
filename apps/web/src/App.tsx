acf5f3975733def38a724b5da5e0fd669616aa92
Marcos Popovich <correodeassus@gmail.com>
2026-01-28 08:03:11 -0300

    feat(buques): editar buque con validación de jefes técnicos

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom"
 import { Layout } from "@/Layout"
 import { Dashboard } from "@/pages/Dashboard"
+import { Buques } from "@/pages/Buques"
 
 export default function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/buques" element={<Buques />} />
       </Routes>
     </Layout>
   )
